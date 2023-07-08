8405705f7d75b0ac376ad63d62f833a0055a3105
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-07-08 14:24:10 +0530

    add permutation problems

--- src/com/janhavi/recursion/DiceThrow.java
+++ src/com/janhavi/recursion/DiceThrow.java
@@ -1,2 +1,18 @@
-package com.janhavi.recursion;public class DiceThrow {
+package com.janhavi.recursion;
+
+public class DiceThrow {
+    public static void main(String[] args) {
+        diceFace("", 4);
+    }
+
+    static void diceFace(String p, int target) {
+        if(target == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for(int i = 1; i <= 6 && i <= target; i ++ ) {
+            diceFace(p + i, target - i);
+        }
+    }
 }
--- src/com/janhavi/recursion/Permutations.java
+++ src/com/janhavi/recursion/Permutations.java
@@ -3,6 +3,7 @@ package com.janhavi.recursion;
 public class Permutations {
     public static void main(String[] args) {
         permutations("", "abc");
+        System.out.print(permutationsNumber("", "abcd"));
     }
 
     static void permutations(String p, String up) {
@@ -17,5 +18,23 @@ public class Permutations {
             String s = p.substring(i, p.length());
             permutations(f + ch + s, up.substring(1));
         }
+    }
+
+
+    static int permutationsNumber(String p, String up) {
+        int count = 0;
+        if(up.isEmpty()) {
+
+            return 1;
+        }
+
+        char ch = up.charAt(0);
+        for(int i = 0; i <= p.length(); i ++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count += permutationsNumber(f + ch + s, up.substring(1));
+        }
+
+        return count;
     }
 }
